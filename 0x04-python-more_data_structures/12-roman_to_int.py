e1a95b75e183d9a27083c73efd3e352850899ced
salma <salmamarokti888@gmail.com>
2024-09-20 21:41:32 -0700

    well well

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+#from romanian to int
 def roman_to_int(roman_string):
     romans = {'I': 1, 'V': 5, 'X': 10, 'L':50, 'C': 100, 'D': 500, 'M': 1000,\
 	    'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900}
