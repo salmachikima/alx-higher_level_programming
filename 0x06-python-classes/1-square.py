8f5071fae03701d56eabb3c349e85e1a7526ee4f
salma <salmamarokti888@gmail.com>
2024-10-01 17:04:38 -0700

    1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" class Square that defines a square"""
+
+
+class Square:
+    """ class Square it define a square"""
+    def __init__(self, size):
+        """initialize
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size  #: size of the square
