0391959542c905956dab8b9460147e3b7f6f3f87
salma <salmamarokti888@gmail.com>
2025-01-21 19:06:02 -0800

    nhgjfe

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#Bash script that takes in a URL, sends a request to that URL
-#and displays the size of the body of the response
+# Take URL, send request to it and display the size
 curl -s "$1" | wc -c
