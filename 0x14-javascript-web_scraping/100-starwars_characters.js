2573e350b1f616a910ef0ebc6fa05773cfd14b13
salmachikima <salmamarokti888@gmail.com>
2025-02-11 19:26:32 -0800

    151155

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,15 +1,36 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
+
+const movieId = process.argv[2];
+const baseUrl = 'https://swapi-api.alx-tools.com/api/films/';
+const fullUrl = baseUrl.concat(movieId);
+
+request(fullUrl, (error, response, body) => {
   if (!error) {
     const characters = JSON.parse(body).characters;
-    characters.forEach((character) => {
-      request(character, function (error, response, body) {
+    // Create a variable to store the number of characters processed
+    let charactersProcessed = 0;
+    // Create an empty array to store the character names
+    const characterNames = [];
+    characters.forEach((characterUrl) => {
+      request(characterUrl, (error, response, body) => {
         if (!error) {
-          console.log(JSON.parse(body).name);
+          const charName = JSON.parse(body).name;
+          // Add the character name to the array
+          characterNames.push(charName);
+        }
+        // Increment the charactersProcessed variable
+        charactersProcessed++;
+        // Check if all characters have been processed
+        if (charactersProcessed === characters.length) {
+          // Log the character names when all characters have been processed
+          characterNames.forEach((actor) => {
+            console.log(actor);
+          });
         }
       });
     });
+  } else {
+    console.log(error);
   }
 });
